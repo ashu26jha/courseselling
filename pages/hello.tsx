4981935e1383c5423ebcc14d93797b407a1f2fd6
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-06-27 19:10:31 +0530

    Added vid mic

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,30 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useCeramicContext } from '../context'
 import { authenticateCeramic } from '../utils'
 import { useMoralis, useWeb3Contract } from "react-moralis";
+import { useHuddle01, useEventListener } from '@huddle01/react';
+import {
+  useLobby,
+  useAudio,
+  useVideo,
+  usePeers,
+  useRoom,
+  useLivestream,
+} from "@huddle01/react/hooks";
+import { Video, Audio } from "@huddle01/react/components";
 
 const courseCode = 'BCS';
 
-// const createQuery = `
-// mutation MyMutation {
-//   createCourseDetails(
-//     input: {
-//       content: {
-//         courseCode: "BCS", 
-//         courseName: "BlockChain Basics", 
-//         courseCreator: "did:key:0x74c7b157af4E5418F03eb928DF309cc98CE38E66"
-//       }
-//     }
-//   ) {
-//     document {
-//       id
-//       courseCreator {
-//         id
-//       }
-//     }
-//   }
-// }
-// `
 
 const query = `
   query MyQuery {
@@ -49,12 +39,78 @@ export default function () {
   const { ceramic, composeClient } = clients;
   const { isWeb3Enabled, chainId, account, enableWeb3 } = useMoralis()
   const [user, setUser] = useState(0);
+  const { initialize, isInitialized } = useHuddle01();
+  const [projectId, setProjectId] = useState("GevCAkXtVgG_XGR_N2YeneVWZhtBH18H");
+  const { joinLobby, leaveLobby, isLoading, isLobbyJoined, error } = useLobby();
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [roomId, setRoomId] = useState('');
+  const [session, setSession] = useState(false);
 
-  useEffect(()=>{
-    if(user!=0){
-      console.log(user);
+  const {
+    fetchVideoStream,
+    produceVideo,
+    stopProducingVideo,
+    stopVideoStream,
+    stream: camStream,
+    isProducing: cam,
+    error: camError,
+  } = useVideo();
+  const {
+    fetchAudioStream,
+    stopAudioStream,
+    produceAudio,
+    stopProducingAudio,
+    stream: micStream,
+    isProducing: mic,
+    error: micError,
+    isProducing
+  } = useAudio();
+
+  useEffect(() => {
+    console.log("camera setting");
+    console.log(camStream);
+    console.log(videoRef)
+    if (camStream && videoRef.current) {
+      videoRef.current.srcObject = camStream;
+      produceVideo(camStream);
+      console.log(isProducing)
+      setSession(true);
     }
-  },[user])
+  }, [camStream, videoRef.current]);
+
+  useEffect(() => {
+    console.log("mic setting");
+    if (micStream) {
+      console.log(micStream)
+      produceAudio(micStream);
+      console.log(isProducing)
+    }
+  }, [micStream]);
+
+
+  useEffect(() => {
+    if (user == 1) {
+      const getRoom = async () => {
+        console.log("s")
+        const response = await fetch('/api', {
+          method: 'POST',
+          body: JSON.stringify({}),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await response.json();
+        if (roomId == '') {
+          setRoomId(data.roomId)
+          await joinLobby(data.roomId);
+          console.log("S")
+
+        }
+
+      }
+      getRoom();
+    }
+  }, [user])
 
 
   const handleLogin = async () => {
@@ -69,38 +125,44 @@ export default function () {
     const response: any = (await composeClient.executeQuery(query)).data!.courseDetailsIndex;
     for (var i = 0; i < response.edges.length; i++) {
       const temp = response.edges[i].node;
-      
+
       if (temp.courseCode == courseCode) {
         const connectedDID = 'did:key:' + account.toString()
-        if((temp.courseCreator.id).toLowerCase() == connectedDID){
+        if ((temp.courseCreator.id).toLowerCase() == connectedDID) {
           console.log("Creator it is")
           setUser(1);
         }
-        else{
+        else {
           setUser(2);
         }
-
-
       }
     }
   }
 
-  // async function createDemoCourse(){
-  //   const update = await composeClient.executeQuery(createQuery);
-  //   console.log(update)
-  // }
 
   useEffect(() => {
-
+    initialize(projectId);
     handleLogin()
   }, [])
 
+  function fetching() {
 
+  }
 
   return (
     <>
-      {user == 1 ? <><div className='peer-id'>Creator View</div></> :<></>}
+      {user == 1 ? <><div className='peer-id'>Creator View</div></> : <></>}
       {user == 2 ? <><div className='peer-id'>Student View</div></> : <></>}
+      <div className ='start'>
+        {!session ? <><button className='p-2' onClick={() => { fetchAudioStream(), fetchVideoStream() }}>Start the session!</button></> : <></>}
+      </div>
+      
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        className='vidStream'
+      />
     </>
   )
 }
